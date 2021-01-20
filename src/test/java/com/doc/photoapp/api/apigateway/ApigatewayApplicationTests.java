bf1098bcf9759ba344bb956e3a11ee2001b7ef5d
anDrekon <4594400@gmail.com>
2021-01-20 12:32:13 +0200

    Initial commit API Gateway (Spring cloud)

---
+++ src/test/java/com/doc/photoapp/api/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.photoapp.api.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
